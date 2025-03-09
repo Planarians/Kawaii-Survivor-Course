90602ac7c0b09378b3bf3c35561c4134291f501c
Planarians <lyplanarians@gmail.com>
2025-03-09 18:41:51 +0000

    Add SpriteSorter script for dynamic sprite rendering order

---
+++ .history/Assets/Kawaii Survivor/Scripts/SpriteSorter_20250309181759.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SpriteSorter : MonoBehaviour
+{
+    [Header("Elements")]
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 根据y轴位置设置渲染顺序
+        spriteRenderer.sortingOrder = (int)(transform.position.y * 10f);
+    }
+}
