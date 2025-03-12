3a012912f03c54fa944d741e46e4b9130200ff03
Planarians <lyplanarians@gmail.com>
2025-03-12 17:47:47 +0000

    Refactor Weapon Class with State Management and Enhanced Hit Detection

---
+++ .history/Assets/Kawaii Survivor/Scripts/Weapons/Weapon_20250312150211.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Linq;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [Header("Elements")]
+    [SerializeField] private Transform hitDetectionTransform;
+    [SerializeField] private float hitDetectionRadius = 0.3f;
+
+    [Header("Settings")]
+    [SerializeField] private float range;
+    [SerializeField] private LayerMask enemyMask;
+
+    [Header("Animations")]
+    //目标旋转速度
+    [SerializeField] private float aimLerp = 12f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        AutoAim();
+        Attack();
+
+    }
+
+    private void AutoAim()
+    {
+        Enemy closestEnemy = GetClosestEnemy();
+
+        Vector2 targetUpVector = Vector3.up;
+
+        if (closestEnemy != null)
+        {
+            targetUpVector = (closestEnemy.transform.position - transform.position).normalized;
+        }
+        // 插值旋转
+        transform.up = Vector3.Lerp(transform.up, targetUpVector, Time.deltaTime * aimLerp);
+    }
+
+    private void Attack()
+    {
+        // 检测是否在攻击范围内
+        Enemy[] enemies = Physics2D.OverlapCircleAll(hitDetectionTransform.position, hitDetectionRadius, enemyMask).Select(x => x.GetComponent<Enemy>()).ToArray();
+
+        foreach (Enemy enemy in enemies)
+        {
+            if (enemy != null)
+            {
+                Destroy(enemy.gameObject);
+            }
+        }
+    }
+
+    private Enemy GetClosestEnemy()
+    {
+
+
+
+        Enemy[] enemies = Physics2D.OverlapCircleAll(transform.position, range, enemyMask).Select(x => x.GetComponent<Enemy>()).ToArray();
+
+        int closestEnemyIndex = -1;
+        float minDistance = Mathf.Infinity;
+
+        if (enemies.Length == 0)
+        {
+            transform.up = Vector3.up;
+            return null;
+        }
+
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemies[i].transform.position);
+            if (distanceToEnemy < minDistance)
+            {
+                minDistance = distanceToEnemy;
+                closestEnemyIndex = i;
+            }
+        }
+        // If there is an enemy, set the enemy to the closest enemy
+        if (closestEnemyIndex != -1)
+        {
+
+            return enemies[closestEnemyIndex];
+        }
+        // If there is no enemy, set the weapon to the up vector
+        else
+        {
+
+            return null;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, range);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(hitDetectionTransform.position, hitDetectionRadius);
+    }
+}
